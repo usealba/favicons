7cfa326fb345f5c4864b0aa6262736770dcb6ef0
Paul Haedrich <paul@berrysauce.me>
2023-06-07 15:25:31 +0200

    Changed all returns to WEBP format

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app.mount("/assets", StaticFiles(directory="templates/assets"), name="assets")
 # CDN:      https://cdn-favicons.alba.quest
 # ------------------------------------------
 
-def resize_favicon(input, format):
+def resize_favicon(input):
      # Create a Pillow Image object from the image data
     image = Image.open(io.BytesIO(input))
 
@@ -31,7 +31,7 @@ def resize_favicon(input, format):
     resized_image = image.resize((16, 16))
     
     output_buffer = io.BytesIO()
-    resized_image.save(output_buffer, format=format) # format must be all caps
+    resized_image.save(output_buffer, format="WEBP") # convert to webp
     image_bytes = output_buffer.getvalue()
     
     return image_bytes
@@ -48,8 +48,8 @@ def get_favicon(url: str):
         ico_request = requests.get(f"{url}/favicon.ico")
     except requests.exceptions.ConnectionError:
         print("[ERROR] ConnectionError")
-        with open("no-favicon.png", "rb") as f: 
-            return Response(content=f.read(), media_type="image/png")
+        with open("no-favicon.webp", "rb") as f: 
+            return Response(content=f.read(), media_type="image/webp")
     except requests.exceptions.MissingSchema:
         raise HTTPException(status_code=400, detail="Schema missing from URL") # 400 - Bad Request
     
@@ -62,12 +62,11 @@ def get_favicon(url: str):
         
         if favicon_tag is None:
             print("[ERROR] Favicon tag couldn't be found")
-            with open("no-favicon.png", "rb") as f: 
-                return Response(content=f.read(), media_type="image/png")
+            with open("no-favicon.webp", "rb") as f: 
+                return Response(content=f.read(), media_type="image/webp")
         
         # get favicon's href and type
         favicon_href = favicon_tag.get("href")
-        favicon_type = favicon_tag.get("type")
         
         # if href is relative, make it absolute
         if "http" not in favicon_href:
@@ -75,25 +74,21 @@ def get_favicon(url: str):
         
         favicon_request = requests.get(favicon_href)
         # print(f"[DEBUG] {favicon_type} found for {url}")
-        
-        # standard favicon type is image/png
-        if favicon_type is None:
-            favicon_type = "image/png"
             
-        resized_favicon = resize_favicon(favicon_request.content, favicon_type.split("/")[1])
+        resized_favicon = resize_favicon(favicon_request.content)
         
-        return Response(content=resized_favicon, media_type=favicon_type)
+        return Response(content=resized_favicon, media_type="image/webp")
     else:
         # print(f"[DEBUG] .ico found for {url}")
         
         try:
-            resized_ico = resize_favicon(ico_request.content, "ICO")
+            resized_ico = resize_favicon(ico_request.content)
         except UnidentifiedImageError:
             print("[ERROR] UnidentifiedImageError")
-            with open("no-favicon.png", "rb") as f: 
-                return Response(content=f.read(), media_type="image/png")
+            with open("no-favicon.webp", "rb") as f: 
+                return Response(content=f.read(), media_type="image/webp")
         
-        return Response(content=resized_ico, media_type="image/x-icon")
+        return Response(content=resized_ico, media_type="image/webp")
 
 
 if __name__ == "__main__":
